54375f5d5a551fac13f68deb7e50fc4ee5b9ad6a
Hussain Mousavi <shm379@live.com>
2022-11-14 11:34:41 +0330

    fix:gender

--- app/Builder/Sorts/GenderSort.php
+++ app/Builder/Sorts/GenderSort.php
@@ -9,9 +9,8 @@ class GenderSort implements \Spatie\QueryBuilder\Sorts\Sort
     public function __invoke(Builder $query, bool $descending, string $property)
     {
         $direction = $descending ? 'DESC' : 'ASC';
-        $query->orderByRaw("CASE gender
-            WHEN 1 THEN 'آقا'
-            WHEN 2 THEN 'خانم'
-        END");
+        foreach (['1', '2'] as $gender) {
+            $query->orderByRaw('gender = ? '.$direction, [$gender]);
+        }
     }
 }
