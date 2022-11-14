f8e487a728f842253eb11d6ed7c443ae7d21131e
Hussain Mousavi <shm379@live.com>
2022-11-14 11:37:59 +0330

    fix:gender

--- app/Builder/Sorts/GenderSort.php
+++ app/Builder/Sorts/GenderSort.php
@@ -2,6 +2,7 @@
 
 namespace App\Builder\Sorts;
 
+use App\Enums\UserGender;
 use \Illuminate\Database\Eloquent\Builder;
 
 class GenderSort implements \Spatie\QueryBuilder\Sorts\Sort
@@ -9,7 +10,7 @@ class GenderSort implements \Spatie\QueryBuilder\Sorts\Sort
     public function __invoke(Builder $query, bool $descending, string $property)
     {
         $direction = $descending ? 'DESC' : 'ASC';
-        foreach (['1', '2'] as $gender) {
+        foreach ([UserGender::MALE, UserGender::FEMALE] as $gender) {
             $query->orderByRaw('gender = ? '.$direction, [$gender]);
         }
     }
