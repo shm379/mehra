564060db6818b4713345991572148f75cf523c72
ali alavi <ali.masoudi.alavi@gmail.com>
2022-11-14 11:35:24 +0330

    f

---
+++ resources/js/Components/Ui/Page/HeaderAction.vue
@@ -0,0 +1,14 @@
+<template>
+  <Link
+    class="px-3 py-3 rounded-2xl bg-neutral-100 text-sm text-slate-600 cursor-pointer hover:scale-95 delay-100 hover:ring-red-600 hover:ring-offset-stone-600 hover:shadow-lg hover:shadow-red-100 hover:ring-4 duration-200 ease-in"
+    as="div"
+    :href="route(to)"
+    ><slot></slot
+  ></Link>
+</template>
+
+<script setup>
+const props = defineProps({
+  to: String,
+});
+</script>
