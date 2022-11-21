e38603591a5ae779bf0b4c3fa100fe950bcab473
ali alavi <ali.masoudi.alavi@gmail.com>
2022-11-21 17:57:51 +0330

    fix

--- database/seeders/MediaSeeder.php
+++ database/seeders/MediaSeeder.php
@@ -22,21 +22,21 @@ class MediaSeeder extends Seeder
         $media = Storage::disk('media')->allFiles();
         $products = Product::query()->get();
 
-        foreach ($products as $product){
+        foreach ($products as $product) {
             $media_rand = array_rand($media);
             $array = explode('.', $media[$media_rand]);
-//            if(!end($array)=='mp4'){
-                if(\Illuminate\Support\Facades\File::exists(public_path('media/').$media[$media_rand])){
-                    $product->addMediaFromUrl(URL::to('media/'.$media[$media_rand]))->toMediaCollection('gallery');
-//                }
+            //            if(!end($array)=='mp4'){
+            if (\Illuminate\Support\Facades\File::exists(public_path('media/') . $media[$media_rand])) {
+                $product->addMediaFromUrl(URL::to('media/' . $media[$media_rand]))->toMediaCollection('gallery');
+                //                }
             }
 
-//            if(\Illuminate\Support\Facades\File::exists(public_path('media_copy/').$media[$media_rand])){
-//                for($i=0;$i<3;$i++){
-//                    $media_rand = array_rand($media);
-//                    $product->addMedia(public_path('media_copy/').$media[$media_rand])->toMediaCollection('gallery');
-//                }
-//            }
+            //            if(\Illuminate\Support\Facades\File::exists(public_path('media_copy/').$media[$media_rand])){
+            //                for($i=0;$i<3;$i++){
+            //                    $media_rand = array_rand($media);
+            //                    $product->addMedia(public_path('media_copy/').$media[$media_rand])->toMediaCollection('gallery');
+            //                }
+            //            }
         }
     }
 }
