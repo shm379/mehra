abefd4ffe27a9a4ca7e89baa8d66996097dee705
Hussain Mousavi <shm379@live.com>
2022-12-06 07:17:31 +0330

    fix:books

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,7 @@ Route::middleware(['auth:sanctum','abilities:view-user'])->group(function () {
 /*
  * V1 Without Auth
  */
+Route::post('books', [\App\Http\Controllers\Api\Product\BookController::class, 'index'])->name('books_index');
 Route::apiResource('books', \App\Http\Controllers\Api\Product\BookController::class)->only('index','show');
 Route::get('filters/books', [\App\Http\Controllers\Api\Product\BookController::class,'filters'])->name('filters.books');
 Route::apiResource('awards', \App\Http\Controllers\Api\Product\AwardController::class)->only('index','show');
