51053129a461bb81ddb49e803d952655ff833289
Hussain Mousavi <shm379@live.com>
2022-12-19 19:41:49 +0330

    fix:options

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -26,10 +26,6 @@ class Cors
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
             return Response::make('OK', 200, $headers);
         }
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
-            ->header('Access-Control-Allow-Credentials',' true');
+        return $next($request);
     }
 }
