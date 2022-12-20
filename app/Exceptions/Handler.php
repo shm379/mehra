d31d771a36846f3d570e57d44d6851fda68c08a4
Hussain Mousavi <shm379@live.com>
2022-12-20 16:42:17 +0330

    fix:debuglog

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -109,6 +110,7 @@ class Handler extends ExceptionHandler
     public function render($request, $e)
     {
 //        if(!$request->expectsJson() || !$request->is('api/*')) return parent::render($request, $e);
+        Log::debug($request->fullUrl().' - '.$e->getMessage());
         return $this->handleApiException($request, $e);
     }
 }
