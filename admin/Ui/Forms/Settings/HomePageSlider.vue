004b95c8cd99a2b621abcb8700d54549a1b168d0
SMN <arvand62@gmaie.com>
2022-12-28 18:37:47 +0330

    fix:home

--- admin/Ui/Forms/Settings/HomePageSlider.vue
+++ admin/Ui/Forms/Settings/HomePageSlider.vue
@@ -30,30 +30,31 @@
       </div>
     </div>
     <div class="flex flex-row gap-2 items-end">
-      <div class="w-5/12">
-          <ui-input label="عنوان" v-model="newSlider"></ui-input>
-      </div>
-      <div class="w-6/12">
-        <ui-input label="توضیحات" v-model="newDescription"></ui-input>
-      </div>
-      <div
-        class="w-1/12 flex flex-row cursor-pointer justify-start items-center"
-        @click="add"
-      >
-        <svg
-          width="30"
-          height="31"
-          viewBox="0 0 30 31"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M29.875 15.5C29.875 15.8516 29.5938 16.0625 29.3125 16.0625H15.8125V29.5625C15.8125 29.9141 15.5312 30.1953 15.25 30.1953C14.8984 30.1953 14.6875 29.9141 14.6875 29.5625V16.0625H1.1875C0.835938 16.0625 0.625 15.8516 0.625 15.5703C0.625 15.2188 0.835938 14.9375 1.1875 14.9375H14.6875V1.4375C14.6875 1.15625 14.8984 0.945312 15.25 0.945312C15.5312 0.945312 15.8125 1.15625 15.8125 1.4375V14.9375H29.3125C29.5938 14.9375 29.875 15.2188 29.875 15.5Z"
-            fill="#A3A3A3"
-          />
-        </svg>
-      </div>
-    </div>
+            <div class="w-6/12">
+
+            </div>
+            <div class="w-6/12">
+                <ui-input label="عنوان" v-model="newSlider"></ui-input>
+                <ui-input label="توضیحات" v-model="newDescription"></ui-input>
+            </div>
+            <div
+                class="w-1/12 flex flex-row cursor-pointer justify-start items-center"
+                @click="add"
+            >
+                <svg
+                    width="30"
+                    height="31"
+                    viewBox="0 0 30 31"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M29.875 15.5C29.875 15.8516 29.5938 16.0625 29.3125 16.0625H15.8125V29.5625C15.8125 29.9141 15.5312 30.1953 15.25 30.1953C14.8984 30.1953 14.6875 29.9141 14.6875 29.5625V16.0625H1.1875C0.835938 16.0625 0.625 15.8516 0.625 15.5703C0.625 15.2188 0.835938 14.9375 1.1875 14.9375H14.6875V1.4375C14.6875 1.15625 14.8984 0.945312 15.25 0.945312C15.5312 0.945312 15.8125 1.15625 15.8125 1.4375V14.9375H29.3125C29.5938 14.9375 29.875 15.2188 29.875 15.5Z"
+                        fill="#A3A3A3"
+                    />
+                </svg>
+            </div>
+        </div>
   </div>
 </template>
 
@@ -73,9 +74,10 @@ const sliders = computed({
   },
 });
 function add() {
-    sliders.value.push({
-    slider: newSlider.value,
-    description: newDescription.value,
+  sliders.value.push({
+      mobile_image: null,
+      desktop_image: null,
+      description: newDescription.value,
   });
   newSlider.value = null;
   newDescription.value = null;
