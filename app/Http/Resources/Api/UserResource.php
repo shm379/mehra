6c7be4707c7fc4f62c04e0ab8847282edf859065
Hussain Mousavi <shm379@live.com>
2023-01-17 20:22:03 +0330

    fix:check-media-exists

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -48,7 +48,7 @@ class UserResource extends MehraResource
                 return $this->following()->get();
             }),
             'following_count'=> $this->following()->count(),
-            'image'=> $this->hasMedia('avatar') ? optional(optional($this->getMedia('avatar'))->first())->getUrl('thumbnail') : $this->getDefaultAvatar(),
+            'image'=> !$this->hasMedia('avatar') && !file_exists($this->getFirstMediaPath('avatar','thumbnail')) ? $this->getDefaultAvatar()  : $this->getFirstMediaUrl('avatar','thumbnail'),
         ];
     }
 }
