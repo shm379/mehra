24a4fac5ab027b3db499cf4c7f26e1cf9589246a
Hussain Mousavi <shm379@live.com>
2023-01-19 10:41:21 +0330

    fix:settings

---
+++ app/Http/Resources/Api/Home/Collections0Resource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\Api\Home;
+
+class Collections0Resource extends HomeResource
+{
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=> $this->id,
+            'title'=> $this->title,
+            'items'=> ProductImageResource::collection($this->products()->get())
+        ];
+    }
+
+}
